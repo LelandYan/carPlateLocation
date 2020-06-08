8b0f2078fb4a46b71b4ea1741d3609054d67582c
lelandyan <2721599586@qq.com>
2020-06-08 16:51:24 +0800

    maskcnn

---
+++ MaskRCNN/detectionTest.py
@@ -0,0 +1,153 @@
+import os
+import sys
+import random
+import math
+import re
+import time
+import numpy as np
+import tensorflow as tf
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import skimage
+import skimage.io as io
+import cv2
+
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+
+# Root directory of the project
+ROOT_DIR = os.path.abspath("./")
+
+# Import Mask RCNN
+sys.path.append(os.path.join(ROOT_DIR, "Mask_RCNN"))  # To find local version of the library
+from mrcnn import utils
+from mrcnn import visualize
+from mrcnn.visualize import display_images
+import mrcnn.model as modellib
+from mrcnn.model import log
+import carplate
+
+# Directory to save logs and trained model
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+
+# Path to Ballon trained weights
+# You can download this file from the Releases page
+# https://github.com/matterport/Mask_RCNN/releases
+CARPLATE_WEIGHTS_PATH = "mask_rcnn_carplate_0030.h5"
+
+# 车牌定位
+
+# %%
+
+config = carplate.CarplateConfig()
+ROOT__DATA_DIR = os.path.abspath("../")
+CARPLATE_DIR = os.path.join(ROOT__DATA_DIR, r"dataset\carplate")
+
+
+# %%
+
+# Override the training configurations with a few
+# changes for inferencing.
+class InferenceConfig(config.__class__):
+    # Run detection on one image at a time
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+
+config = InferenceConfig()
+
+# Device to load the neural network on.
+# Useful if you're training a model on the same
+# machine, in which case use CPU and leave the
+# GPU for training.
+DEVICE = "/gpu:0"  # /cpu:0 or /gpu:0
+
+# Inspect the model in training or inference modes
+# values: 'inference' or 'training'
+TEST_MODE = "inference"
+
+
+def get_ax(rows=1, cols=1, size=16):
+    """Return a Matplotlib Axes array to be used in
+    all visualizations in the notebook. Provide a
+    central point to control graph sizes.
+
+    Adjust the size attribute to control how big to render images
+    """
+    _, ax = plt.subplots(rows, cols, figsize=(size * cols, size * rows))
+    return ax
+
+
+# Load validation dataset
+dataset = carplate.CarplateDataset()
+dataset.load_carplate(CARPLATE_DIR, "val")
+
+# Must call before using the dataset
+dataset.prepare()
+
+print("Images: {}\nClasses: {}".format(len(dataset.image_ids), dataset.class_names))
+# Load Mask-RCNN Model
+# Create model in inference mode
+with tf.device(DEVICE):
+    model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
+# Load weights
+weights_path = CARPLATE_WEIGHTS_PATH
+print("Loading weights", weights_path)
+model.load_weights(weights_path, by_name=True)
+# Run Detection
+image_id = random.choice(dataset.image_ids)
+image, image_meta, gt_class_id, gt_bbox, gt_mask = \
+    modellib.load_image_gt(dataset, config, image_id, use_mini_mask=False)
+info = dataset.image_info[image_id]
+print("image ID: {}.{} ({}) {}".format(info["source"], info["id"], image_id,
+                                       dataset.image_reference(image_id)))
+
+visualize.display_instances(image, gt_bbox, gt_mask, gt_class_id, dataset.class_names)
+results = model.detect([image], verbose=1)
+ax = get_ax(1)
+r = results[0]
+visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'],
+                            dataset.class_names, r['scores'], ax=ax,
+                            title="Predictions")
+# # 1.提取车牌区域，只取第一个车牌
+# mask = r['masks'][:, :, 0].astype(np.uint8)
+# plt.show()
+# _, contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+# cnt = contours[0]
+# epsilon = 0.1 * cv2.arcLength(cnt, True)
+# approx = cv2.approxPolyDP(cnt, epsilon, True)
+# approx = approx.squeeze()
+#
+# if approx.shape == (4, 2):
+#     box = np.zeros_like(approx)
+#     box[:, 0] = approx[:, 1]
+#     box[:, 1] = approx[:, 0]
+# else:
+#     rect = cv2.minAreaRect(np.array(np.nonzero(mask)).T)
+#     box = cv2.boxPoints(rect).astype(np.int)
+#
+# y0, x0 = box.min(axis=0)
+# y1, x1 = box.max(axis=0)
+# img = image[y0:y1, x0:x1]
+# io.imshow(img)
+# plt.show()
+# box[:, 0] -= y0
+# box[:, 1] -= x0
+# # 调整box顺序，从左上角开始，逆时针转动
+# i0 = (box[:, 0] + box[:, 1]).argmin()
+# box = box[[i0, (i0 + 1) % 4, (i0 + 2) % 4, (i0 + 3) % 4]]
+# plt.scatter(box[:, 1], box[:, 0], c='r')
+# # 2.矫正车牌
+# h = np.max([box[1][0] - box[0][0], box[2][0] - box[3][0]])
+# w = np.max([box[2][1] - box[1][1], box[3][1] - box[0][1]])
+# box2 = np.array([(0, 0), (h, 0), (h, w), (0, w)])
+# M = cv2.getPerspectiveTransform(box[:, ::-1].astype(np.float32), box2[:, ::-1].astype(np.float32))
+# img = cv2.warpPerspective(img, M, (w, h))
+# img = cv2.resize(img, (220, 70))
+# io.imshow(img)
+# plt.show()
--- README.md
+++ README.md
@@ -1,2 +1,74 @@
-# carPlateLocation
-using  Masn_RCNN for car palte location 
+# 车牌定位功能
+## 数据来源 
+
+**本项目使用的数据集为246张中文车牌数据集, 数据来源:**https://gitee.com/easypr/EasyPR/tree/master/resources/image/general_test
+
+**数据标注工具: **
+
+**labelImg 项目地址**:https://github.com/tzutalin/labelImg
+
+**VGG Image Annotator VIA**:http://www.robots.ox.ac.uk/~vgg/software/via/
+
+## 项目介绍
+
+**本项目将以下提供三种模型可以对车牌进行定位**
+
+1. 使用传统数字图像处理技术(Python-Opencv)，通过图像预处理->图像增强->二值化->边缘检测->图像提取分割
+2. 使用已经训练COO数据的目标检测模型(**Object Detection:YOLOv3**)，做迁移学习，对已经训练过COCO数据的模型卷积层权重参数进行微调，从而得到车牌定位功能的新模型。
+3. 使用已经训练实例分割气球模型(**Instance Segmentation:Mask R-CNN**)，做迁移学习，对已经训练过COCO数据模型参数进行微调，从而得到车牌定位功能的新模型。
+
+## 传统数字图像处理方法
+
+
+
+## 目标检测模型 ---YOLOv3
+
+1. 需要安装的Yolov3模型框架，模型原项目地址:https://github.com/AlexeyAB/darknet (这里选择使用这个版本是为了兼容window平台)可按照其步骤进行安装。
+
+2. 该项目和代码使用Google的Colab提供免费GPU运行
+
+3. 本项目使用数据格式采用Yolov3数据格式，可以通过项目的transform对 VIA标注图片结果via_region_data.json进行转化,转化好的所需数据集为 obj.zip  obj.name obj.data
+
+4. YOLOv3 在COCO数据集训练卷积层权重参数文件 darknet53.conv.74 可以将其放在Goole Drive根目录下下载: http://pjreddie.com/media/files/darknet53.conv.74
+
+5. 该项目配置时候需要设置Google的Drive和colab路径，在Google Drive中的建立Yolo3文件夹，并在Yolo3文件夹中放入所需要的数据（obj.zip  obj.name obj.data Yolov3配置文件yolov3_custom2.cfg 分割训练数据文件generate_train.py ）以及建立backup文件夹(有用保存模型训练时候权重参数).---这里所需文件会提供
+
+   文件结构如：Google Drivve  ->  yolo3  ->  obj.zip
+
+   ​								                                      ->  obj.name
+
+   ​											                          ->  obj.data
+
+   ​											                          ->  yolov3_custom2.cfg
+
+   ​																	   -> generate_train.py 
+
+   ​											                           -> backup
+
+   
+
+6. 该车牌定位YOLOv3模型训练的权重结果在:https://drive.google.com/file/d/1--Kmf45XzvjNPnrBXeVkunZolRn-N6J9/view?usp=sharing
+
+7. 模型所需数据和配置文件在:YOLOv3/yolo3
+
+8. 模型主要代码为:**licensePlateLocation.ipynb**
+
+9. 模型所需数据格式转为:transform.py
+
+## 实例分割气球模型---Mask R-CNN
+
+1. 需要下载的Mask R-CNN模型框架，模型原项目地址:https://github.com/matterport/Mask_RCNN 可按照其步骤进行安装所需要类库
+2. 该项目和代码使用Google的Colab提供免费GPU运行。
+3. 这里注意因为Google的Colab提供tf和keras环境都已经升级到2.x了,如果直接在colab上运行会报版本的错误。我们需要通过命令 **%tensorflow_version 1.4** 在引入tf框架前加上，来限制tf的版本，通过 **! pip install q keras==2.1**命令在引入keras前来限制kears版本。
+4. 本项目使用数据格式采用VIA方式标注图片结果。
+5. 该项目配置时候需要设置Google的Drive和colab路径，在Google Drive中的建立Mask_RCNN文件夹和dataset文件夹，在Mask_RCNN文件夹下加入模型训练和检验文件inspect_carplate_model.ipynb和经过COCO数据训练权重系数mask_rcnn_coco.5(https://github.com/matterport/Mask_RCNN/releases) ,在dataset文件下放入所需数据carplate.zip (https://github.com/airxiechao/simple-car-plate-recognition/blob/master/dataset/carplate.zip) ，在Colab Notebooks中放入mrcnn文件夹(这里在原项目mrcnn中加入caplate.py文件)--- 这里所需文件会提供
+6. 该车牌定位Mask R-CNN模型训练的权重结果在:https://drive.google.com/file/d/110PH2IwIwRg94equVqor-Ip8sR1fD760/view?usp=sharing
+7. 模型主要代码为:**Mask-RCNN/licensePlateLocation.ipynb**
+8. 模型所需的功能代码为Colab Notebook\mrcnn
+9. 该模型所需数据集为dataset文件夹(这里根据上面提供网上进行下载)
+10. 模型检验代码detectionTest.py
+
+
+
+​				
+
